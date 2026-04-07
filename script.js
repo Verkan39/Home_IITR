a57691c1b057e716df13444e5d043da3950d2464
Verkan <vedmeharia@gmail.com>
2026-04-07 12:52:55 +0530

    Added responsiveness over all screen sizes

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 document.getElementById("first_of_top").addEventListener("click", function () {
     this.classList.toggle("active");
 });
+let index=0;
+const container=document.querySelector(".a-card-set");
+const cards = document.querySelectorAll(".a-card");
+
+function showSlide(i){
+    index=(i+cards.length)%cards.length;
+
+    container.scrollTo({
+        left:cards[index].offsetLeft,
+        behavior:"smooth"
+    });
+}
+
+document.querySelector(".next").addEventListener("click",()=>{
+    showSlide(index+1);
+});
+
+document.querySelector(".prev").addEventListener("click",()=>{
+    showSlide(index-1);
+});
+
