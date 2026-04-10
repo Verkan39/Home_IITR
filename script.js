73e25ee724cda9442a94079115c7c36d21c37ea7
Verkan <vedmeharia@gmail.com>
2026-04-11 01:16:58 +0530

    Add milestone carousel

--- script.js
+++ script.js
@@ -1,38 +1,21 @@
 document.getElementById("first_of_top").addEventListener("click", function () {
     this.classList.toggle("active");
 });
-// let index=0;
-// const container=document.querySelector(".a-card-set");
-// const cards = document.querySelectorAll(".a-card");
-
-// function showSlide(i){
-//     index=(i+cards.length)%cards.length;
-
-//     container.scrollTo({
-//         left:cards[index].offsetLeft,
-//         behavior:"smooth"
-//     });
-// }
-
-// document.querySelector(".next").addEventListener("click",()=>{
-//     showSlide(index+1);
-// });
-
-// document.querySelector(".prev").addEventListener("click",()=>{
-//     showSlide(index-1);
-// });
-
 const nextBtn=document.getElementById('anext');
 const prevBtn=document.getElementById('aprev');
 const all_cards=document.getElementById('acs');
 nextBtn.addEventListener('click', ()=>{
     const cardwidth=all_cards.querySelector('.a-card').clientWidth;
     all_cards.scrollBy({left:cardwidth+25, behavior: 'smooth'});
+    clearInterval(slideInterval);
+    slideInterval=setInterval(autoscroll,3000);
 });
 
 prevBtn.addEventListener('click', () => {
   const cardWidth = all_cards.querySelector('.a-card').clientWidth;
   all_cards.scrollBy({ left: -cardWidth-25, behavior: 'smooth' });
+  clearInterval(slideInterval1);
+  slideInterval=setInterval(autoscroll,3000);
 });
 
 let index=0;
@@ -78,3 +61,65 @@ eventNames.forEach((name)=>{
 let slideInterval=setInterval(autoscroll, 3000);
 let slideInterval1=setInterval(autoscroll_events,3000);
 
+const years=document.querySelectorAll('.aise');
+function updatedesign(indexm1){
+    years.forEach(year=> year.classList.remove('active'));
+    years.forEach(year => {
+        year.querySelector('.dot img').src = "picture/Ellipse 20.svg"; // default
+    });
+    years[indexm1].querySelector('.dot img').src = "picture/yellow_dot.svg";
+    if (years[indexm1]) {
+        years[indexm1].classList.add('active');
+    }
+}
+
+const yearsp=document.querySelectorAll('.year');
+function updatephone(indexm2){
+    yearsp.forEach(year=>year.classList.remove('active'));
+    if(yearsp[indexm2]){
+        yearsp[indexm2].classList.add('active');
+    }
+}
+
+const nextBtnm=document.getElementById('next_btn_m');
+const prevBtnm=document.getElementById('prev_btn_m');
+const all_cardsm=document.getElementById('mcs');
+nextBtnm.addEventListener('click', ()=>{
+    const cardwidth=all_cardsm.querySelector('.m-card').clientWidth;
+    indexm++;
+    if (indexm >= slidesm.length) indexm = 0;
+    all_cardsm.scrollBy({left:cardwidth, behavior: 'smooth'});
+    clearInterval(slideIntervalm);
+    slideIntervalm=setInterval(autoscrollm,3000);
+    updatedesign(indexm);
+    updatephone(indexm);
+});
+
+prevBtnm.addEventListener('click', () => {
+  const cardWidth = all_cardsm.querySelector('.m-card').clientWidth;
+  indexm--;
+  if (indexm < 0) indexm = slidesm.length - 1;
+  all_cardsm.scrollBy({ left: -cardWidth, behavior: 'smooth' });
+  clearInterval(slideIntervalm);
+  slideIntervalm=setInterval(autoscrollm,3000);
+  updatedesign(indexm);
+  updatephone(indexm);
+});
+
+let indexm=0;
+const slidesm=document.querySelectorAll('.m-card');
+function autoscrollm(){
+    indexm++;
+    if(indexm>=slidesm.length){
+        indexm=0;
+        // all_cards.scrollTo({ left: 0, behavior: 'smooth' });
+    }
+    const cardwidth=all_cardsm.querySelector('.m-card').clientWidth;
+    all_cardsm.scrollTo({left:(cardwidth)*indexm, behavior: 'smooth'});
+    updatedesign(indexm);
+    updatephone(indexm);
+}
+
+let slideIntervalm=setInterval(autoscrollm,3000);
+
+updatedesign(0);
